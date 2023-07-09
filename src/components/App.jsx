e8990be52cf7830ab323b0d8ca40fe4fcd6827c2
Hanna Rogalska <hanrogalska@gmail.com>
2023-07-09 11:29:29 +0200

    phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
-export const App = () => {
+import ContactForm from './ContactForm';
+import Filter from './Filter';
+import ContactList from './ContactList';
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="main">
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <Filter />
+      <ContactList />
     </div>
   );
-};
+}
